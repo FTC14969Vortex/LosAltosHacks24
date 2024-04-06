67c4f26bd4da55728e4fcadb2c77dffbe5c03d0b
Shashvat Mehta <shashvatsmehta@gmail.com>
2024-04-06 16:48:37 -0700

    Writing output to a file

--- MyModel/DetectionTest.py
+++ MyModel/DetectionTest.py
@@ -8,12 +8,14 @@ import cv2
 filePath = '/Users/shash/Documents/Vortex/LosAltosHacks24/MyModel/keras_model.h5'
 labelPath = '/Users/shash/Documents/Vortex/LosAltosHacks24/MyModel/labels.txt'
 
+file = open("/Users/shash/Documents/Vortex/LosAltosHacks24/output.txt", "w")
 #Initialize the classifier with the file and label paths
 classifier = Classifier(filePath, labelPath)
 
 #Get the camera
 camera = cv2.VideoCapture(0)  # Initialize video capture
 
+#Text on the screen
 #font
 font = cv2.FONT_HERSHEY_SIMPLEX  
 # position 
@@ -28,20 +30,27 @@ thickness = 2
 run = True
 while run:
     _, img = camera.read()  # Capture frame-by-frame
+    
     prediction = classifier.getPrediction(img, False)
     
     output = str(prediction[2])
     
+    file.write(output)
+    print("Output written to Output.txt")
+    
     img = cv2.putText(img, output, org, font, fontScale, color, thickness, cv2.LINE_AA)
     
-    print(prediction)  # Print prediction result
-
+    #Flip the image
+    cv2.flip(img, 1)
+    
+    #Display the image
     cv2.imshow("Image", img)
 
     
     # Wait for a key press to close window
     if cv2.waitKey(1) == 27:
         run = False
+        file.close()
             
 camera.release()
 cv2.destroyAllWindows()
