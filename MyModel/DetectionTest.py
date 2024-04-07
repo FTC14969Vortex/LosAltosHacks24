845d2efa9a4a57e72d3c44f83355223ce2a2737f
Shashvat Mehta <shashvatsmehta@gmail.com>
2024-04-06 20:43:31 -0700

    Testing pygame window and key presses

--- MyModel/DetectionTest.py
+++ MyModel/DetectionTest.py
@@ -2,7 +2,10 @@ import os
 os.environ["TF_USE_LEGACY_KERAS"] = "1"
 from cvzone.ClassificationModule import Classifier
 import cv2
-import keyboard
+import pygame
+
+#Set up pygame
+pygame.init()
 
 #Update these file paths here as the model and label files are added
 #Copy file paths by right clicking on the file, pressing option and selecting "Copy as Pathname"
@@ -27,34 +30,52 @@ color = (255, 0, 0)
 # Line thickness of 2 px 
 thickness = 2
 
+#Get the inital image
+_, img = camera.read()  # Capture frame-by-frame
+
+#Set up the pygame window
+X, Y, _ = img.shape
+screen = pygame.display.set_mode((X, Y))
+pygame.display.set_caption('ASL Translator')
+
 run = True
 while run:
     _, img = camera.read()  # Capture frame-by-frame
+
     
     prediction = classifier.getPrediction(img, False)
     
     output = str(prediction[2])
+    
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.putText(img, output, org, font, fontScale, color, thickness, cv2.LINE_AA)
+
+    
+    # Convert the image to Pygame surface
+    height, width, _ = img.shape
+    pygame_img = pygame.image.fromstring(img.tobytes(), (width, height), 'RGB')
         
-    if keyboard.is_pressed('enter'):  # if key 'enter' is pressed 
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_RETURN]:
+        print('Enter key pressed')
         file = open(outputFilePath, "w")
         file.write(output)
         file.close()
         
-    file.write(output)
+    # Display the image
+    screen.blit(pygame_img, (0, 0))
+    pygame.display.flip()
     
-    img = cv2.putText(img, output, org, font, fontScale, color, thickness, cv2.LINE_AA)
+    # img = cv2.putText(img, output, org, font, fontScale, color, thickness, cv2.LINE_AA)
     
     #Flip the image
     cv2.flip(img, 1)
     
-    #Display the image
-    cv2.imshow("Image", img)
-
-    
+        
     # Wait for a key press to close window
-    if cv2.waitKey(1) == 27:
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_ESCAPE]:
         run = False
-        file.close()
             
 camera.release()
 cv2.destroyAllWindows()
